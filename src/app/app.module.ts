4127538d9cb130fcffc4f41df26d9dd45ff81034
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-05-24 15:30:43 -0300

    Agregar componente de busqueda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+
+const appRoutes: Routes = [
+  {path: 'buscar', component: BuscarComponent},
+  {path: '', redirectTo: '/buscar', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
