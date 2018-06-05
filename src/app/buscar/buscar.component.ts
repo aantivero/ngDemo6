1db80226fcbe5b5c397def377bb3a7674b87267a
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-06-05 09:19:20 -0300

    Subscripcion del componente buscar

--- src/app/buscar/buscar.component.ts
+++ src/app/buscar/buscar.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BuscarService, Person} from '../shared/buscar/buscar.service';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
   styleUrls: ['./buscar.component.css']
 })
-export class BuscarComponent implements OnInit {
+export class BuscarComponent implements OnInit, OnDestroy {
   query: string;
   searchResults: Array<Person>;
+  sub: Subscription
 
-  constructor(private buscarService: BuscarService) { }
+  constructor(private buscarService: BuscarService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      if (params['term']) {
+        this.query = decodeURIComponent(params['term']);
+        this.buscar();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   buscar(): void {
