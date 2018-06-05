a09bee9fbf904b880b5a1bcef57309ba61a46d02
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-06-05 10:07:52 -0300

    Testing BuscarService, testear el servicio que devuelve todos

--- src/app/shared/buscar/buscar.service.spec.ts
+++ src/app/shared/buscar/buscar.service.spec.ts
@@ -1,15 +1,44 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed, inject, getTestBed } from '@angular/core/testing';
 import { BuscarService } from './buscar.service';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('BuscarService', () => {
+  let injector: TestBed;
+  let service: BuscarService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [BuscarService]
     });
+
+    injector = getTestBed();
+    service = injector.get(BuscarService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should be created', inject([BuscarService], (service: BuscarService) => {
+  it('debería devolver todos ', () => {
+    const dummyData = [
+      {name: 'Elon Musk'},
+      {name: 'Jack Ma'}
+    ];
+    service.getAll().subscribe((people: any) => {
+      expect(people.length).toBe(2);
+      expect(people[0].name).toBe('Elon Musk');
+      expect(people).toEqual(dummyData);
+    });
+
+    const req = httpMock.expectOne('assets/data/persona.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyData);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('debería crearse el servicio', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
