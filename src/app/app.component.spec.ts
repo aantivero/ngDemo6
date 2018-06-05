515c2e351d726bd53fbd6bdf34ea5a386970fa1f
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2018-06-05 09:46:35 -0300

    Testing, utilizar routerTestingModule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule} from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [RouterTestingModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -22,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngDemo6!');
+    expect(compiled.querySelector('h1').textContent).toContain('Inicio app!');
   }));
 });
